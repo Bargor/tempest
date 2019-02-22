d852eebcad3ec027acb0c9a0e2f5ef696d69a6d2
Karol Kontny <barolek@gmail.com>
2019-02-22 18:42:24 +0100

    Added application sources and proper Cmakes

---
+++ src/tempest/tempest/engine.cpp
@@ -0,0 +1,57 @@
+// This file is part of Tempest-engine-GL project
+// Author: Karol Kontny
+
+#include "engine.h"
+
+#include <application/event_processor.h>
+#include <application/input_processor.h>
+#include <application/main_window.h>
+
+#include <util/variant.h>
+
+#include <engine/engine.h>
+
+namespace tst {
+namespace application {
+
+    simulation_engine::simulation_engine(event_processor& eventProcessor,
+                                         input_processor& inputProcessor,
+                                         main_window& mainWindow,
+                                         data_loader& dataLoader)
+        : m_eventProcessor(eventProcessor)
+        , m_inputProcessor(inputProcessor)
+        , m_mainWindow(mainWindow)
+        , m_dataLoader(dataLoader)
+        , m_renderingEngine(std::make_unique<engine::rendering_engine>(m_dataLoader))
+        , m_timer()
+        , m_frameCount(0)
+        , m_shouldClose(false) {
+        auto close_callback = [&](const event::arguments&) { m_shouldClose = true; };
+        m_eventProcessor.subscribe(core::variant_index<event::arguments, event::closed>(), std::move(close_callback));
+    }
+
+    simulation_engine::~simulation_engine() {}
+
+    void simulation_engine::run() {
+        auto lastFrameTime = std::chrono::microseconds::zero();
+        while (!m_shouldClose) {
+            m_timer.start();
+            main_loop();
+            m_timer.stop();
+
+            lastFrameTime = m_timer.get_time();
+            m_timer.reset();
+            m_eventProcessor.create_event(event{event::time{lastFrameTime}});
+            m_frameCount++;
+        }
+    }
+
+    void simulation_engine::main_loop() {
+        m_inputProcessor.process_events();
+        m_eventProcessor.process_events();
+        m_renderingEngine->frame();
+        m_mainWindow.end_frame();
+    }
+
+} // namespace application
+} // namespace tst
---
+++ src/tempest/tempest/main.cpp
@@ -0,0 +1,59 @@
+// This file is part of Tempest-engine-GL project
+// Author: Karol Kontny
+
+#include <GLFW/glfw3.h>
+
+#include "application.h"
+
+#include <application/argument_parser.h>
+#include <application/event_processor.h>
+#include <application/glfw_exception.h>
+#include <application/main_window.h>
+#include <thread/main_thread.h>
+#include <array>
+#include <cstdlib>
+#include <device/monitor.h>
+#include <fmt/printf.h>
+
+namespace tst {
+
+void init_Glfw() {
+    assert(std::this_thread::get_id() == tst::core::main_thread::get_id());
+    if (!glfwInit()) {
+        fmt::printf("Can't initialize Glfw\n");
+        std::exit(EXIT_FAILURE);
+    }
+}
+
+auto init_monitor() {
+    assert(std::this_thread::get_id() == tst::core::main_thread::get_id());
+    GLFWmonitor* glfwMonitor = glfwGetPrimaryMonitor();
+    if (glfwMonitor) {
+        return std::make_unique<device::monitor>(*glfwMonitor);
+    } else {
+        throw tst::application::glfw_exception("Can't get monitor");
+    }
+}
+
+void deinit_Glfw() {
+    assert(std::this_thread::get_id() == tst::core::main_thread::get_id());
+    glfwTerminate();
+}
+} // namespace tst
+
+int main(int argc, const char** argv) {
+    const tst::application::program_params params = tst::application::parse_arguments(argc, argv);
+    tst::init_Glfw();
+    try {
+        auto monitor = tst::init_monitor();
+        tst::application::application app(params, std::move(monitor), "Tempest");
+        app.run();
+    } catch (tst::application::glfw_exception& ex) {
+        const char* description;
+        glfwGetError(&description);
+        fmt::printf("%s, error: %s\n", ex.what(), description);
+        std::exit(EXIT_FAILURE);
+    } catch (...) { fmt::printf("Unknown error\n"); }
+    tst::deinit_Glfw();
+    return 0;
+}
