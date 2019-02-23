2d7a5d78b588063ee9c50eb418d72eeb258d816b
Karol Kontny <barolek@gmail.com>
2019-02-23 12:43:34 +0100

    Added single consumer multiple producer queue and tests, not fully working yet

--- src/core/container/queue.h
+++ src/core/container/queue.h
@@ -7,6 +7,7 @@
 #include <atomic>
 #include <core.h>
 #include <optional>
+#include <util/bitpack.h>
 
 namespace tst {
 namespace core {
@@ -20,8 +21,12 @@ namespace core {
 
     public:
         spsc_queue() noexcept;
-
-        bool try_push(const T& element) noexcept;
+		
+		bool empty() noexcept;
+		
+		bool try_push(const T& element) noexcept;
+		// This one has overwrite semantics when queue is full, will overwrite oldest item
+        void push(const T& element) noexcept;       
         std::optional<T> try_pop() noexcept;
 
     private:
@@ -37,6 +42,14 @@ namespace core {
     template<typename T, size_t Size>
     TST_INLINE spsc_queue<T, Size>::spsc_queue() noexcept : m_front(0), m_back(0) {
     }
+	
+	template<typename T, size_t Size>
+	TST_INLINE bool spsc_queue<T, Size>::empty() noexcept {
+		index_type frontPos = m_front.load(std::memory_order_relaxed);
+		index_type backPos = m_back.load(std::memory_order_relaxed);
+		
+		return backPos == frontPos;
+	}
 
     template<typename T, size_t Size>
     TST_INLINE bool spsc_queue<T, Size>::try_push(const T& element) noexcept {
@@ -50,6 +63,15 @@ namespace core {
         m_front.store(newFront, std::memory_order_release);
 
         return true;
+    }
+	
+    template<typename T, size_t Size>
+	TST_INLINE void spsc_queue<T, Size>::push(const T& element) noexcept {
+		index_type frontPos = m_front.load(std::memory_order_acquire);
+		index_type newFront = next_pos(frontPos);
+		
+	    m_buffer[frontPos] = element;
+        m_front.store(newFront, std::memory_order_release);
     }
 
     template<typename T, size_t Size>
@@ -68,6 +90,100 @@ namespace core {
 
     template<typename T, size_t Size>
     constexpr typename spsc_queue<T, Size>::index_type spsc_queue<T, Size>::next_pos(index_type idx) noexcept {
+        return ++idx == bufferSize ? 0 : idx;
+    }
+	
+    // single producer multiple consumer lock free queue based on ring buffer
+    template<typename T, std::size_t Size>
+    class spmc_queue {
+    public:
+        using index_type = std::uint32_t;
+        using store_type = std::uint64_t;
+
+    public:
+        spmc_queue() noexcept;
+
+        bool empty() noexcept;
+
+        bool try_push(const T& element) noexcept;
+        // This one has overwrite semantics when queue is full, will overwrite oldest item
+        void push(const T& element) noexcept;
+        std::optional<T> try_pop() noexcept;
+
+    private:
+        constexpr index_type next_pos(index_type idx) noexcept;
+
+    private:
+        union index_descriptor {
+
+            std::atomic<store_type> full;
+            std::array<std::atomic<index_type>, 2> half;
+
+            index_descriptor(store_type num) : full(num) {}
+            index_descriptor(index_type back, index_type front) : half{{back, front}} {}
+
+        };
+
+
+    private:
+        static constexpr std::size_t bufferSize = Size + 1;
+        std::array<T, bufferSize> m_buffer;
+        index_descriptor m_positions;
+
+        static_assert(sizeof(index_descriptor) == sizeof(uint64_t));
+    };
+
+    template<typename T, size_t Size>
+    TST_INLINE spmc_queue<T, Size>::spmc_queue() noexcept : m_positions(0) {
+    }
+
+    template<typename T, size_t Size>
+    TST_INLINE bool spmc_queue<T, Size>::empty() noexcept {
+        store_type positions = m_positions.full.load(std::memory_order_acquire);
+        index_type front = get_high_bits<store_type, index_type>(positions);
+        index_type back = get_low_bits<store_type, index_type>(positions);
+
+        return front == back;
+    }
+	
+    template<typename T, size_t Size>
+    TST_INLINE bool spmc_queue<T, Size>::try_push(const T& element) noexcept {
+        store_type positions = m_positions.full.load(std::memory_order_acquire);
+        index_type front = get_high_bits<store_type, index_type>(positions);
+        index_type back = get_low_bits<store_type, index_type>(positions);
+
+        index_type next_front = next_pos(front);
+
+        if (next_front == back) {
+            return false;
+        }
+
+        m_buffer[front] = element;
+        m_positions.half[1].store(next_front, std::memory_order_release);
+        return true;
+    }
+
+    template<typename T, size_t Size>
+    TST_INLINE std::optional<T> spmc_queue<T, Size>::try_pop() noexcept {
+        while (true) {
+            store_type positions = m_positions.full.load(std::memory_order_acquire);
+            index_type front = get_high_bits<store_type, index_type>(positions);
+            index_type back = get_low_bits<store_type, index_type>(positions);
+
+            if (front == back) {
+                return std::nullopt;
+            }
+
+            auto value = m_buffer[back];
+
+            if (m_positions.half[0].compare_exchange_strong(back, next_pos(back), std::memory_order_acq_rel)) {
+                return value;
+            }
+        }
+    }
+
+    template<typename T, size_t Size>
+    constexpr typename spmc_queue<T, Size>::index_type spmc_queue<T, Size>::next_pos(index_type idx) noexcept {
         return ++idx == bufferSize ? 0 : idx;
     }
 
--- test/unit/core/container/queue_test.cpp
+++ test/unit/core/container/queue_test.cpp
@@ -23,6 +23,26 @@ TEST(spsc_queue, test_push_than_pop) {
     }
 }
 
+TEST(spsc_queue, test_empty) {
+    constexpr std::int32_t size = 31;
+
+    spsc_queue<std::int32_t, size> queue;
+
+    EXPECT_TRUE(queue.empty());
+}
+
+TEST(spsc_queue, test_not_empty) {
+    constexpr std::int32_t size = 31;
+
+    spsc_queue<std::int32_t, size> queue;
+
+    EXPECT_TRUE(queue.empty());
+
+    queue.try_push(1);
+
+    EXPECT_FALSE(queue.empty());
+}
+
 TEST(spsc_queue, test_push_fail) {
     constexpr std::int32_t size = 31;
 
@@ -136,4 +156,183 @@ TEST(spsc_queue, test_push_pop_interleaved_2) {
 
     producer.join();
     consumer.join();
+}
+
+
+TEST(spmc_queue, test_empty) {
+    constexpr std::int32_t size = 31;
+
+    spmc_queue<std::int32_t, size> queue;
+
+    EXPECT_TRUE(queue.empty());
+}
+
+TEST(spmc_queue, test_not_empty) {
+    constexpr std::int32_t size = 31;
+
+    spmc_queue<std::int32_t, size> queue;
+
+    EXPECT_TRUE(queue.empty());
+
+    queue.try_push(1);
+
+    EXPECT_FALSE(queue.empty());
+}
+
+TEST(spmc_queue, test_push_than_pop) {
+    constexpr std::int32_t size = 31;
+
+    spmc_queue<std::int32_t, size> queue;
+
+    for (std::int32_t i = 0; i < size; ++i) {
+        EXPECT_TRUE(queue.try_push(i));
+    }
+
+    for (std::int32_t i = 0; i < size; ++i) {
+        auto res = queue.try_pop();
+        EXPECT_EQ(i, res.value());
+    }
+}
+
+TEST(spmc_queue, test_push_fail) {
+    constexpr std::int32_t size = 31;
+
+    spmc_queue<std::int32_t, size> queue;
+
+    for (std::int32_t i = 0; i < size; ++i) {
+        EXPECT_TRUE(queue.try_push(i));
+    }
+
+    EXPECT_FALSE(queue.try_push(32));
+
+    for (std::int32_t i = 0; i < size; ++i) {
+        auto res = queue.try_pop();
+        EXPECT_EQ(i, res.value());
+    }
+}
+
+TEST(spmc_queue, test_pop_fail) {
+    constexpr std::int32_t size = 32;
+
+    spmc_queue<std::int32_t, size> queue;
+
+    for (std::int32_t i = 0; i < size; ++i) {
+        EXPECT_TRUE(queue.try_push(i));
+    }
+
+    for (std::int32_t i = 0; i < size; ++i) {
+        auto res = queue.try_pop();
+        EXPECT_EQ(i, res.value());
+    }
+
+    EXPECT_FALSE(queue.try_pop());
+}
+
+TEST(spmc_queue, test_push_pop_interleaved) {
+    constexpr std::int32_t size = 1023;
+    std::vector<std::int32_t> items(size, 0);
+
+    spmc_queue<std::int32_t, size> queue;
+
+    std::atomic<std::int32_t> count = 0;
+
+    std::promise<void> enabler;
+
+    auto notifier = enabler.get_future().share();
+
+    auto push_queue = [&]() {
+        notifier.wait();
+
+        for (std::int32_t i = 0; i < size; ++i) {
+            EXPECT_TRUE(queue.try_push(i));
+            items[i] = 1;
+        }
+    };
+
+    auto pop_queue = [&]() {
+        notifier.wait();
+
+        auto last = 0;
+        while (count < size) {
+            auto res = queue.try_pop();
+            if (res) {
+                EXPECT_LE(last, res.value());
+                last = res.value();
+                count++;
+                items[last] = 2;
+            }
+        }
+    };
+
+    std::thread producer(push_queue);
+    std::thread consumer_1(pop_queue);
+    std::thread consumer_2(pop_queue);
+
+    enabler.set_value();
+
+    producer.join();
+    consumer_1.join();
+    consumer_2.join();
+
+    for (auto i : items) {
+        EXPECT_EQ(i, 2);
+    }
+}
+
+TEST(spmc_queue, test_push_pop_interleaved_2) {
+    constexpr std::int32_t size = 1023;
+    constexpr std::int32_t mult = 2;
+    std::vector<std::int32_t> items(size * mult, 0);
+
+    spmc_queue<std::int32_t, size> queue;
+
+    std::promise<void> enabler;
+
+    std::atomic<std::int32_t> pop_count = 0;
+
+    auto notifier = enabler.get_future().share();
+
+    auto push_queue = [&]() {
+        notifier.wait();
+
+        std::int32_t count = 0;
+
+        while (count < mult * size) {
+            auto res = queue.try_push(count);
+            if (res) {
+                items[count] = 1;
+                ++count;
+            }
+        }
+    };
+
+    auto pop_queue = [&]() {
+        notifier.wait();
+
+        auto last = 0;
+
+        while (pop_count < 2 * size) {
+            auto res = queue.try_pop();
+            if (res) {
+                EXPECT_LE(last, res.value());
+                last = res.value();
+                pop_count++;
+                items[last] = 2;
+            }
+        }
+    };
+
+    std::thread producer(push_queue);
+    std::thread consumer_1(pop_queue);
+    std::thread consumer_2(pop_queue);
+
+    enabler.set_value();
+
+    producer.join();
+    consumer_1.join();
+    consumer_2.join();
+
+    for (auto i : items) {
+        EXPECT_EQ(i, 2);
+    }
 }
