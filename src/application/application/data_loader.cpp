81290e8754a454f09647c17b788b61be10d952a6
Karol Kontny <barolek@gmail.com>
2019-03-30 17:24:57 +0100

    Added implementations of loading shaders code to tdata loader

--- src/application/application/data_loader.cpp
+++ src/application/application/data_loader.cpp
@@ -5,23 +5,67 @@
 
 #include "data_exception.h"
 
+#include <fmt/format.h>
+#include <fmt/printf.h>
+#include <fstream>
+
 namespace tst {
 namespace application {
 
-    data_loader::data_loader() { m_searchPaths.emplace_back("/"); }
+    data_loader::data_loader() noexcept {
+        m_searchPaths.emplace_back("/");
+    }
 
-    data_loader::data_loader(std::initializer_list<std::filesystem::path> pathList) : m_searchPaths(pathList) {}
+    data_loader::data_loader(std::initializer_list<std::filesystem::path> pathList) noexcept : m_searchPaths(pathList) {
+    }
 
-    data_loader::~data_loader() {}
+    data_loader::~data_loader() {
+    }
 
-    std::string data_loader::load_shader_source(const std::string_view&, const std::string_view&) const {
-        return std::string{};
+    std::string data_loader::load_text_file(const std::filesystem::path& path) const {
+        std::ifstream input(path, std::ios::in);
+        if (input) {
+            std::string contents;
+            input.seekg(0, std::ios::end);
+            contents.resize(input.tellg());
+            input.seekg(0, std::ios::beg);
+            input.read(&contents[0], contents.size());
+            input.close();
+            return (contents);
+        }
+        throw data_exception(fmt::sprintf("Can't load file: %s", path.string()));
+    }
+
+    std::vector<char> data_loader::load_shader_bytecode(const std::filesystem::path& path) const {
+        std::ifstream input(path, std::ios::in | std::ios::binary);
+        if (input) {
+            std::vector<char> contents;
+            input.seekg(0, std::ios::end);
+            contents.resize(input.tellg());
+            input.seekg(0, std::ios::beg);
+            input.read(&contents[0], contents.size());
+            input.close();
+            return (contents);
+        }
+        throw data_exception(fmt::sprintf("Can't load file: %s", path.string()));
     }
 
     void data_loader::load_obj_model(const std::string_view&) const {
     }
 
-    void data_loader::add_search_path(std::string&& path) noexcept { m_searchPaths.emplace_back(std::move(path)); }
+    std::optional<std::filesystem::path> data_loader::find_file(const std::string& name) const {
+        for (auto path : m_searchPaths) {
+            path += name;
+            if (std::filesystem::exists(path)) {
+                return path;
+            }
+        }
+        return std::nullopt;
+    }
+
+    void data_loader::add_search_path(std::string&& path) noexcept {
+        m_searchPaths.emplace_back(std::move(path));
+    }
 
 } // namespace application
 } // namespace tst
--- src/application/application/data_loader.h
+++ src/application/application/data_loader.h
@@ -5,20 +5,23 @@
 #include <filesystem>
 #include <string_view>
 #include <vector>
+#include <optional>
 
 namespace tst {
 namespace application {
 
     class data_loader {
     public:
-        data_loader();
-        data_loader(std::initializer_list<std::filesystem::path> pathList);
+        data_loader() noexcept;
+        data_loader(std::initializer_list<std::filesystem::path> pathList) noexcept;
 
         virtual ~data_loader();
 
     public:
-        virtual std::string load_shader_source(const std::string_view& name, const std::string_view& format) const;
+        virtual std::string load_text_file(const std::filesystem::path& path) const;
+        virtual std::vector<char> load_shader_bytecode(const std::filesystem::path& path) const;
         virtual void load_obj_model(const std::string_view& name) const;
+        virtual std::optional<std::filesystem::path> find_file(const std::string& name) const;
 
     public:
         void add_search_path(std::string&& path) noexcept;
