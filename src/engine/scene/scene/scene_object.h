39191d2b720e1b22fcc4ad44b3a75eb583ba7f10
Karol Kontny <barolek@gmail.com>
2019-07-08 00:28:01 +0200

    Added material to scene

--- src/engine/scene/scene/scene_object.h
+++ src/engine/scene/scene/scene_object.h
@@ -2,6 +2,18 @@
 // Author: Karol Kontny
 #pragma once
 
+#include <resources/vertex_buffer.h>
+#include <resources/index_buffer.h>
+
 namespace tst {
-namespace scene {}
+namespace scene {
+
+	class scene_object {
+    public:
+    private:
+        std::vector<engine::resources::vertex_buffer> m_vertices;
+        std::vector<engine::resources::index_buffer> m_indices;
+	};
+
+}
 } // namespace tst
