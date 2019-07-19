ef20be9ae69c9c5dc9e7c4b8d1eaf79f4215a27c
Kontny Karol <barolek@gmail.com>
2019-07-19 14:59:10 +0200

    Opengl placeholders for compilation

---
+++ src/engine/engine-GL/engine/resources/index_buffer.h
@@ -0,0 +1,13 @@
+// This file is part of Tempest-engine project
+// Author: Karol Kontny
+#pragma once
+
+namespace tst {
+namespace engine {
+    namespace opengl {
+		template<typename IndexType>
+        class index_buffer {
+		};
+	}
+}
+}
---
+++ src/engine/engine-GL/engine/resources/uniform_buffer.h
@@ -0,0 +1,12 @@
+// This file is part of Tempest-engine project
+// Author: Karol Kontny
+#pragma once
+
+namespace tst {
+namespace engine {
+    namespace opengl {
+        class uniform_buffer {
+		};
+	}
+}
+}
