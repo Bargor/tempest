6646df0afcc3699176170080a1306d1dd769e4da
Karol Kontny <barolek@gmail.com>
2019-07-23 00:12:38 +0200

    GL compilation fixes

---
+++ src/rendering/engine-GL/engine-impl/resources/vertex_format.h
@@ -0,0 +1,21 @@
+// This file is part of Tempest-engine-GL project
+// Author: Karol Kontny
+
+#pragma once
+
+#include <engine-base/resources/vertex_format.h>
+
+namespace tst {
+namespace engine {
+    namespace opengl {
+        class vertex_format : public base::vertex_format {
+        public:
+            using super = base::vertex_format;
+
+            vertex_format();
+
+        private:
+        };
+    } // namespace opengl
+} // namespace engine
+} // namespace tst
