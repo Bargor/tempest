5f6b9d76c97b6818655eaba3fd8422951f82652f
Karol Kontny <barolek@gmail.com>
2019-07-25 20:29:25 +0200

    Fixed test range

--- test/perf/core/queue_test.cpp
+++ test/perf/core/queue_test.cpp
@@ -73,7 +73,7 @@ namespace core {
         state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range(0)));
     }
 
-    BENCHMARK(BM_spinlock_queue_int)->RangeMultiplier(2)->Range(size, 8 << 4)->Threads(2);
+    BENCHMARK(BM_spinlock_queue_int)->RangeMultiplier(2)->Range(size, 8 << 18)->Threads(2);
     BENCHMARK(BM_spsc_queue_int)->RangeMultiplier(2)->Range(size, 8 << 18)->Threads(2);
 	
     static void BM_spsc_queue_s512(benchmark::State& state) {
@@ -135,7 +135,7 @@ namespace core {
         state.SetBytesProcessed(int64_t(state.iterations()) * int64_t(state.range(0)));
     }
 	
-	BENCHMARK(BM_spinlock_queue_s512)->RangeMultiplier(2)->Range(size, 8 << 16)->Threads(2);
+	BENCHMARK(BM_spinlock_queue_s512)->RangeMultiplier(2)->Range(size, 8 << 18)->Threads(2);
     BENCHMARK(BM_spsc_queue_s512)->RangeMultiplier(2)->Range(size, 8 << 18)->Threads(2);
 
     static void BM_spinlock_queue_push(benchmark::State& state) {
