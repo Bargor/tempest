d4a712b51e84a70a535e60874746144e177f734c
Kontny Karol <barolek@gmail.com>
2019-08-02 11:24:54 +0200

    Implemented unfiform and vertex buffer based on buffer

--- src/rendering/engine-GL/engine-impl/resources/vertex_buffer.cpp
+++ src/rendering/engine-GL/engine-impl/resources/vertex_buffer.cpp
@@ -7,15 +7,15 @@ namespace engine {
     namespace opengl {
 
         vertex_buffer::vertex_buffer(const vertex_format&,
-                                     std::vector<vertex>&& vertices)
-            : m_vertices(std::move(vertices)) {
+                                     std::vector<vertex>&& vertices) : buffer(vertices.size() * sizeof(vertex))
+            , m_vertices(std::move(vertices)) {
         }
 
         vertex_buffer::~vertex_buffer() {
         }
 
         vertex_buffer::vertex_buffer(vertex_buffer&& other) noexcept
-            : m_format(other.m_format), m_vertices(std::move(other.m_vertices)) {
+            : buffer(std::move(other)), m_format(other.m_format), m_vertices(std::move(other.m_vertices)) {
         }
 
     }
