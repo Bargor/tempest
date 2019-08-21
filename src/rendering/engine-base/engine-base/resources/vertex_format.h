de2dcdd7f3caff872ceb8d8ab820405ad4a5f567
Karol Kontny <ext-karol.kontny@here.com>
2019-08-21 16:42:01 +0200

    Reformatted vertex format and added primitive topology

--- src/rendering/engine-base/engine-base/resources/vertex_format.h
+++ src/rendering/engine-base/engine-base/resources/vertex_format.h
@@ -9,53 +9,63 @@
 
 namespace tst {
 namespace engine {
-	
-	struct vertex {
+
+    struct vertex {
         glm::vec2 pos;
         glm::vec3 color;
     };
-	
-	namespace base {
 
-    class vertex_format {
-    public:
-        enum class format {
-            invalid,
-            float1,
-            float2,
-            float3,
-            float4,
-            int1,
-            int2,
-            int3,
-            int4,
-            uint,
-            uint2,
-            uint3,
-            uint4
+    namespace base {
+
+        class vertex_format {
+        public:
+            enum class format {
+                invalid,
+                float1,
+                float2,
+                float3,
+                float4,
+                int1,
+                int2,
+                int3,
+                int4,
+                uint,
+                uint2,
+                uint3,
+                uint4
+            };
+
+            enum class location { position, normal, texcoord, binormal };
+
+            enum class primitive_topology {
+                point_list,
+                line_list,
+                line_strip,
+                triangle_list,
+                triangle_strip,
+                triangle_fan
+            };
+
+            struct attribute_descriptor {
+                vertex_format::location location;
+                vertex_format::format format;
+                std::uint32_t offset;
+                std::uint32_t stride;
+                std::uint32_t divisor;
+            };
+
+        public:
+            vertex_format();
+
+            void add_attribute(
+                location location, format format, std::uint32_t offset, std::uint32_t stride, std::uint32_t divisor);
+            const std::vector<attribute_descriptor>& get_atttibutes() const noexcept;
+
+        private:
+            std::vector<attribute_descriptor> m_attributes;
+            primitive_topology m_topology;
         };
 
-        enum class location { position, normal, texcoord, binormal };
-
-        struct attribute_descriptor {
-            vertex_format::location location;
-            vertex_format::format format;
-            std::uint32_t offset;
-            std::uint32_t stride;
-            std::uint32_t divisor;
-        };
-
-    public:
-        vertex_format();
-
-        void add_attribute(
-            location location, format format, std::uint32_t offset, std::uint32_t stride, std::uint32_t divisor);
-        const std::vector<attribute_descriptor>& get_atttibutes() const noexcept;
-
-    private:
-        std::vector<attribute_descriptor> m_attributes;
-    };
-	
-	}
+    } // namespace base
 } // namespace engine
 } // namespace tst
