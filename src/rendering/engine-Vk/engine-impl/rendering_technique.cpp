4ea1ed62d0422647836a023205e6cbb98c32b164
Karol Kontny <ext-karol.kontny@here.com>
2019-08-22 15:56:00 +0200

    Added rendering technique prototype

---
+++ src/rendering/engine-Vk/engine-impl/rendering_technique.cpp
@@ -0,0 +1,10 @@
+// This file is part of Tempest-engine project
+// Author: Karol Kontny
+
+#include "rendering_technique.h"
+
+namespace tst {
+namespace engine {
+    namespace vulkan {} // namespace vulkan
+} // namespace engine
+} // namespace tst
---
+++ src/rendering/engine-Vk/engine-impl/rendering_technique.h
@@ -0,0 +1,15 @@
+// This file is part of Tempest-engine project
+// Author: Karol Kontny
+#pragma once
+
+namespace tst {
+namespace engine {
+    namespace vulkan {
+
+        class rendering_technique {
+        public:
+        private:
+        };
+    } // namespace vulkan
+} // namespace engine
+} // namespace tst
