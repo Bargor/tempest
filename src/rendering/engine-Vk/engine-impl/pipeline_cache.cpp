91014b3b129493a9758da6c9916695f9e4e6b4ab
Karol Kontny <ext-karol.kontny@here.com>
2019-08-29 12:46:16 +0200

    Added pipeline cache

---
+++ src/rendering/engine-Vk/engine-impl/pipeline_cache.cpp
@@ -0,0 +1,9 @@
+// This file is part of Tempest-engine project
+// Author: Karol Kontny
+#include "pipeline_cache.h"
+
+namespace tst {
+namespace engine {
+    namespace vulkan {}
+} // namespace engine
+} // namespace tst
