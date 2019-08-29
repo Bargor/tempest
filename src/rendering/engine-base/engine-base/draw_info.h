504023687e8c917787413b4c5954cf960db4f0f0
Karol Kontny <ext-karol.kontny@here.com>
2019-08-29 12:38:56 +0200

    Added base draw info

---
+++ src/rendering/engine-base/engine-base/draw_info.h
@@ -0,0 +1,16 @@
+// This file is part of Tempest-engine project
+// Author: Karol Kontny
+
+#include <string>
+
+namespace tst {
+namespace engine {
+    namespace base {
+
+        struct draw_info {
+            std::string techniqueName;
+        };
+
+    } // namespace base
+} // namespace engine
+} // namespace tst
