bc7e2040d59a726f6313ee93cad869ea0073ea57
Karol Kontny <barolek@gmail.com>
2020-09-01 17:27:10 +0200

    Fix for newest Vulkan

--- src/rendering/engine-Vk/engine-impl/gpu_info.cpp
+++ src/rendering/engine-Vk/engine-impl/gpu_info.cpp
@@ -38,7 +38,7 @@ namespace engine {
             default:
                 super::deviceVendor = device_vendor::other;
             }
-            super::deviceName = deviceProperties.deviceName;
+            super::deviceName = std::move(deviceProperties.deviceName);
 
             maxIndexValue = deviceProperties.limits.maxDrawIndexedIndexValue;
             maxFramebufferWidth = deviceProperties.limits.maxFramebufferWidth;
