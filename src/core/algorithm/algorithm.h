9748453ab2afe6f7110917c8e0f5b09453fc814b
Karol Kontny <barolek@gmail.com>
2020-09-17 21:33:28 +0200

    Implemented for_each_adjacent

--- src/core/algorithm/algorithm.h
+++ src/core/algorithm/algorithm.h
@@ -9,7 +9,7 @@
 namespace tst {
 
 template<typename InputIt1, typename InputIt2, typename Pred>
-bool includes(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2, Pred pred) {
+constexpr bool includes(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2, Pred pred) {
     for (; first2 != last2; ++first2) {
         auto it = first1;
         for (; it != last1; ++it) {
@@ -22,5 +22,17 @@ bool includes(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2, 
         }
     }
     return true;
+}
+
+template<typename InputIt, typename Pred>
+constexpr Pred for_each_adjacent(InputIt first, InputIt last, Pred pred) { // perform function for each consecutive
+                                                                            // pair of elements [_First, _Last)
+    if (std::distance(first, last) < 2) return pred;
+    auto second = std::next(first);
+    for (; second != last; ++first, ++second) {
+        pred(*first, *second);
+    }
+
+    return pred;
 }
 }
