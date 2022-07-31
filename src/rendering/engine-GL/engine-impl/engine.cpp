942c5c2f8d6b463eda692b00daeaf1d59066290e
Karol Kontny <barolek@gmail.com>
2022-07-31 17:17:57 +0200

    Compilation fixes pt. 3

--- src/rendering/engine-GL/engine-impl/engine.cpp
+++ src/rendering/engine-GL/engine-impl/engine.cpp
@@ -18,7 +18,8 @@ namespace engine {
             glewExperimental = GL_TRUE;
             GLenum err = glewInit();
             if (err != GLEW_OK) {
-                fmt::printf("Can't initialize OpenGL, error %s\n", std::string_view(glewGetErrorString(err)));
+                fmt::printf("Can't initialize OpenGL, error %s\n",
+                            std::string_view(reinterpret_cast<const char*>(glewGetErrorString(err))));
                 std::exit(EXIT_FAILURE);
             }
         }
